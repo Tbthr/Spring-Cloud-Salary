99088e0b7020a220f150c13c625684d666c10759
zj <1965907019@qq.com>
2021-01-17 09:16:22 +0800

    provider_log

--- provider_log/src/main/java/com/salary/controller/LogController.java
+++ provider_log/src/main/java/com/salary/controller/LogController.java
@@ -1,4 +1,34 @@
 package com.salary.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.salary.mapper.LogMapper;
+import com.salary.model.Log;
+import com.salary.service.LogService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
+@RequestMapping("/log")
 public class LogController {
+    @Resource
+    private LogService logService;
+
+    @GetMapping("/getAllLog")
+    public PageInfo<Log> getAllLog(@RequestBody HashMap<String, Object> map, @RequestParam int page,@RequestParam int rows) {
+        return logService.getAllLog(map, page, rows);
+    }
+
+    @GetMapping("/getAllLogOrigin")
+    public List<Log> getAllLogOrigin(@RequestBody HashMap<String, Object> map) {
+        return logService.getAllLogOrigin(map);
+    }
+
+    @PostMapping("/deleteById")
+    public int deleteById(@RequestParam Integer id) {
+        return logService.deleteById(id);
+    }
 }
