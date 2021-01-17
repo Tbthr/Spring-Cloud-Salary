80a77db729330b08fa54f7e5e5c226799fad2084
Tbthr <1598622578@qq.com>
2021-01-17 09:11:22 +0800

    temp

--- provider_bill/src/main/java/com/salary/controller/BillController.java
+++ provider_bill/src/main/java/com/salary/controller/BillController.java
@@ -1,4 +1,89 @@
 package com.salary.controller;
 
+import com.github.pagehelper.PageInfo;
+import com.salary.model.Bill;
+import com.salary.model.BillMap;
+import com.salary.service.BillService;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+
+@RequestMapping("/bill")
 public class BillController {
+    @Resource
+    private BillService billService;
+
+    /**
+     * 查询账单
+     *
+     * @param map  查询条件
+     * @param page 当前页码
+     * @param rows 每页条数
+     * @return 筛选分页后的账单信息
+     */
+    @GetMapping("/getInfo")
+    public PageInfo<Bill> getInfo(HashMap<String, Object> map, int page, int rows) {
+        return billService.getInfo(map, page, rows);
+    }
+
+    /**
+     * 查询账单
+     *
+     * @param map 查询条件
+     * @return 筛选后的账单信息
+     */
+    @GetMapping("/getInfoOrigin")
+    public List<Bill> getInfoOrigin(HashMap<String, Object> map) {
+        return billService.getInfoOrigin(map);
+    }
+
+    /**
+     * 查询账单
+     *
+     * @param date   日期
+     * @param userId 工号
+     * @return 账单
+     */
+    @GetMapping("/getBillById")
+    public Bill getBillById(String date, @Length(min = 10, max = 10, message = "账号长度为 10 位") String userId) {
+        return billService.getBillById(date + '%', userId);
+    }
+
+    /**
+     * 获取最新账单
+     *
+     * @return 账单列表
+     */
+    @GetMapping("/getLatestBills")
+    public List<BillMap> getLatestBills(String userId) {
+        return billService.getLatestBills(userId);
+    }
+
+    /**
+     * 插入账单
+     *
+     * @param bill 账单信息
+     * @return 成功：1 失败：0
+     */
+    @PostMapping("/insertBill")
+    public int insertBill(@Valid Bill bill) {
+        return billService.insertBill(bill);
+    }
+
+    /**
+     * 更新账单
+     *
+     * @param bill 账单信息
+     * @return 成功：1 失败：0
+     */
+    @PostMapping("/updateBill")
+    public int updateBill(@Valid Bill bill) {
+        return billService.updateBill(bill);
+    }
 }
--- provider_department/src/main/java/com.salary/controller/DepartmentController.java
+++ provider_department/src/main/java/com.salary/controller/DepartmentController.java
@@ -1,4 +1,25 @@
 package com.salary.controller;
 
+import com.salary.model.Department;
+import com.salary.service.DepartmentService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RequestMapping("/depart")
 public class DepartmentController {
+    @Resource
+    private DepartmentService departmentService;
+
+    /**
+     * 获取部门
+     *
+     * @return 部门列表
+     */
+    @GetMapping("/getAllDeparts")
+    public List<Department> getAllDeparts() {
+        return departmentService.getAllDeparts();
+    }
 }
