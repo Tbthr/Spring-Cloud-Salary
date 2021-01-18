047608c36bc38c22027e19a17f064abeab74e21c
Tbthr <1598622578@qq.com>
2021-01-18 10:14:17 +0800

    V3.0

--- eureka7002/src/main/java/com/salary/Eureka2Application.java
+++ eureka7002/src/main/java/com/salary/Eureka2Application.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaApplication {
+public class Eureka2Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaApplication.class, args);
+        SpringApplication.run(Eureka2Application.class, args);
     }
 
 }
